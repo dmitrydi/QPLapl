4943cc94d59ea6ffe0139c4743fcded13d188000
dmitrydi <d.dmitrook@gmail.com>
2021-06-10 16:11:22 +0300

    clean 1

--- QPLapl/mainwindow.h
+++ QPLapl/mainwindow.h
@@ -5,18 +5,18 @@
 #include <QDebug>
 #include <QTableView>
 #include <QStandardItemModel>
-#include "unicombobox.h"
-#include "labelmapper.h"
-#include "picmanager.h"
-#include "noteditabledelegate.h"
-#include "wellmanager.h"
-#include "pqgraphwindow.h"
+#include <QSpacerItem>
 #include <QFileDialog>
 #include <QTextStream>
+//#include "unicombobox.h"
+//#include "labelmapper.h"
+#include "picmanager.h"
+#include "noteditabledelegate.h"
+//#include "wellmanager.h"
+#include "pqgraphwindow.h"
 #include "tablewellschedule.h"
 #include "qgrid1d.h"
 #include "gridplot.h"
-#include <QSpacerItem>
 #include "abstractlineinput.h"
 #include "interfacemaps.h"
 #include "pqtview.h"
@@ -41,72 +41,69 @@ public slots:
     void SavePQData();
     void SaveGridData();
 private:
-
-
-    friend class MainWindowData;
     Ui::MainWindow *ui;
     PQGraphWindow *graphWin;
     GridPlot *gridWin;
-    UniComboBox *uComboUnits;
-    UniComboBox *uComboWellType;
-    UniComboBox *uComboAreaShape;
-    UniComboBox *uComboBoundaryConditions;
-    UniComboBox *uComboCalcMode;
+//    UniComboBox *uComboUnits;
+//    UniComboBox *uComboWellType;
+//    UniComboBox *uComboAreaShape;
+//    UniComboBox *uComboBoundaryConditions;
+//    UniComboBox *uComboCalcMode;
     PicManager *picMan;
-    WellManager *wellManager;
-    QVector<DataPoint> PQTData;
-    QList<Matrix3DV> GridData;
-    void setupUComboUnits();
-    void setupUComboWellType();
-    void setupUComboAeaShape();
-    void setupUComboBoundaryConditions();
-    void setupUComboCalcMode();
-    void _setupPayUnits(const QString&);
-    void setupPayUnitsLabels();
-    void setupWellParamsLabels();
-    void _setupWellParamsUnits(const QString&);
-    void setupDrainageAreaLabels();
-    void _setupWellParamsVisibility(const QString& wellType);
-    void _setXfLineVisible(bool visible);
-    void _setFcdLineVisible(bool visible);
-    void _setLhLineVisible(bool visible);
-    void _setNumFracLineVisible(bool visible);
-    void _setRwLineVisible(bool visible);
-    void _setupDrainageAreaVisibility(const QString& area, const QString& welltype);
-    void _setupDrainageAreaUnits(const QString& units);
-    void _setXeLineVisible(bool visible);
-    void _setXwLineVisible(bool visible);
-    void _setYeLineVisible(bool visible);
-    void _setYwLineVisible(bool visible);
-    void _setZwLineVisible(bool visible);
-    void _setReLineVisible(bool visible);
-    void _setLiqRateLineVisible(bool visible);
-    void _setWellPresLineVisible(bool visible);
-    void _setupeCalcModeUnits(const QString& units);
-    void _setupCalcModeVisibility(const QString& calcmode);
-    void setupPicMan();
-    void setupCalcModeLabels();
+//    WellManager *wellManager;
+//    QVector<DataPoint> PQTData;
+//    QList<Matrix3DV> GridData;
+//    void setupUComboUnits();
+//    void setupUComboWellType();
+//    void setupUComboAeaShape();
+//    void setupUComboBoundaryConditions();
+//    void setupUComboCalcMode();
+//    void _setupPayUnits(const QString&);
+//    void setupPayUnitsLabels();
+//    void setupWellParamsLabels();
+//    void _setupWellParamsUnits(const QString&);
+//    void setupDrainageAreaLabels();
+//    void _setupWellParamsVisibility(const QString& wellType);
+//    void _setXfLineVisible(bool visible);
+//    void _setFcdLineVisible(bool visible);
+//    void _setLhLineVisible(bool visible);
+//    void _setNumFracLineVisible(bool visible);
+//    void _setRwLineVisible(bool visible);
+//    void _setupDrainageAreaVisibility(const QString& area, const QString& welltype);
+//    void _setupDrainageAreaUnits(const QString& units);
+//    void _setXeLineVisible(bool visible);
+//    void _setXwLineVisible(bool visible);
+//    void _setYeLineVisible(bool visible);
+//    void _setYwLineVisible(bool visible);
+//    void _setZwLineVisible(bool visible);
+//    void _setReLineVisible(bool visible);
+//    void _setLiqRateLineVisible(bool visible);
+//    void _setWellPresLineVisible(bool visible);
+//    void _setupeCalcModeUnits(const QString& units);
+//    void _setupCalcModeVisibility(const QString& calcmode);
+//    void setupPicMan();
+//    void setupCalcModeLabels();
     // table schedule model
-    TableWellSchedule *PQSchedule;
-    TableWellSchedule *GridSchedule;
-    void SetupPQSchedule();
-    void SetupGridSchedule();
-    //-- Interaction with WellManager
-    void setWellManagerData();
-    void runWellManagerPQ();
-    void runWellManagerGrid();
-    void refreshAndRunDialog();
-    //-- Spatial Calc
-    QGrid1D *testGrid;
-    void SetupGrids();
-    QGrid1D *gNxLeft, *gNxWell, *gNxRight;
-    QGrid1D *gNyBottom, *gNyTop, *gNyWell;
-    QGrid1D *gNzBottom, *gNzTop;
-    QGrid1D *gNxBetween, *gNyBetween;
-    void testAbstractLineInput();
-    TextLineInput *textLine;
-    ComboLineinput *comboLine;
-    ComboLineinput *comboVisible;
+//    TableWellSchedule *PQSchedule;
+//    TableWellSchedule *GridSchedule;
+//    void SetupPQSchedule();
+//    void SetupGridSchedule();
+//    //-- Interaction with WellManager
+//    void setWellManagerData();
+//    void runWellManagerPQ();
+//    void runWellManagerGrid();
+//    void refreshAndRunDialog();
+//    //-- Spatial Calc
+//    QGrid1D *testGrid;
+//    void SetupGrids();
+//    QGrid1D *gNxLeft, *gNxWell, *gNxRight;
+//    QGrid1D *gNyBottom, *gNyTop, *gNyWell;
+//    QGrid1D *gNzBottom, *gNzTop;
+//    QGrid1D *gNxBetween, *gNyBetween;
+//    void testAbstractLineInput();
+//    TextLineInput *textLine;
+//    ComboLineinput *comboLine;
+//    ComboLineinput *comboVisible;
 
     // new objects
     WellController *wellController;
